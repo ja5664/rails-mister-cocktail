bbac510a954ac9d3735bc137b7cc9fecc4306b53
Joe Alexander <ja5664@gmail.com>
2017-11-10 11:11:22 +0000

    initial css templates added - stying work required

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+list = open(url).read
+
+parsed = JSON.parse(list)
+
+puts "Clearing the db..."
+Ingredient.destroy_all
+
+puts "Seeding the db..."
+
+ingredients = parsed["drinks"]
+
+ingredients.each do |i|
+  value = i["strIngredient1"]
+  Ingredient.create!({name: value})
+  puts "#{value} has been added to the db"
+end
+
+puts "Seeds created"
---
+++ test/controllers/dose_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DoseControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
